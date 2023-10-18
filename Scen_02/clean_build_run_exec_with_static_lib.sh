e96e9f13424ab5eb88ee90f51c33013f45e5603f
XiaoLi <leonpa1987@gmail.com>
2023-10-18 23:24:29 +0800

    udpate

---
+++ Scen_02/clean_build_run_exec_with_static_lib.sh
@@ -0,0 +1,20 @@
+#!/bin/bash
+
+set -x
+
+clear
+
+#Build a static lib and run with it
+echo "Building a static lib and run it."
+make clean
+make static_lib
+ls ./build
+
+BUILD_DIR=$PWD/src
+B_FUNCTION_DIR=$BUILD_DIR/B_function
+C_FUNCTION_DIR=$B_FUNCTION_DIR/C_function
+echo $BUILD_DIR
+echo $B_FUNCTION_DIR
+echo $C_FUNCTION_DIR
+gcc $BUILD_DIR/main.c $BUILD_DIR/A_function.c -o ./build/ExecutableFileWithStaticLib -I$BUILD_DIR -I$B_FUNCTION_DIR -I$C_FUNCTION_DIR -L./build ./build/StaticLib.a
+./build/ExecutableFileWithStaticLib
